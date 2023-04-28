7a977887d57d6d338ebb1d55110a12fc8e51d2da
vakandi <wael.bousfira@protonmail.com>
2023-04-28 02:52:18 +0100

    update

--- run.sh
+++ run.sh
@@ -10,9 +10,29 @@ api_ios="xxxx"
 link="http://$ip:$port"
 datee=$(date "+%Y_%m_%d_%H:%M")
 
+echo "\033[1;34m Do you want to use this folder below as the shared folder ?\n /home/$USER/Videos  ? \n Type "y" or "n" and press ENTER\033[0m"
+read automatic_folder
+if [ $automatic_folder == "y" ]
+then
+	rm -rf server/public
+	ln -s /home/$USER/Videos server/public
+	echo "\033[1;34m The folders links has been made\033[0m"
+elif [ $automatic_folder == "n" ]
+then
+	echo "\033[1;34m Enter the path folder you want to share and type ENTER\n\033[0m"
+	read choice
+	if [ -z $choice ]
+	then
+		echo "Choice empty.. re-run the script"
+		exit
+	else
+		rm -rf server/public
+		ln -s $choice server/public
+		continue
+	fi
+fi
+
 echo "Notifications sending"
-
-
 #Android Push notfications 
 curl http://xdroid.net/api/message\?k\=$api_android\&t\=MOVIE+PHP+SERVER+READY\&c\=$datee+$link\&u\=$link 
 
